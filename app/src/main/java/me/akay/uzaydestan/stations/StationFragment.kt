b1f23fe857f74a8616d5730ffef9962578855912
aabbasi <aabolfazlit@gmail.com>
2021-05-28 19:56:43 +0300

    Change station fragment name

--- app/src/main/java/me/akay/uzaydestan/stations/StationFragment.kt
+++ app/src/main/java/me/akay/uzaydestan/stations/StationFragment.kt
@@ -35,7 +35,7 @@ class StationFragment : DaggerFragment() {
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val view = inflater.inflate(R.layout.fragment_main, container, false)
+        val view = inflater.inflate(R.layout.fragment_station, container, false)
 
         val snapHelper = RecyclerViewSnapHelper(object : OnSelectedItemChange {
             override fun onSelectedItemChange(position: Int) {
